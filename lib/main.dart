7ced8f673e6d4a212a666f30b1f464da21871469
saci zakaria <sacideveloppeur@gmail.com>
2021-09-19 11:58:16 +0200

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home:Scaffold(
+        appBar: AppBar(),
 
       ),
     );

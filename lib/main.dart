22a05d18e59f373054196f61b0d6d69443df94e1
saci zakaria <sacideveloppeur@gmail.com>
2021-09-19 11:53:25 +0200

    StatefulWidget const

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
 
   @override
   State<MyApp> createState() => _MyAppState();

05ec9a062990e79e06b8c24cba06df086337afdc
saci zakaria <sacideveloppeur@gmail.com>
2021-09-21 10:35:39 +0200

    Question/Answer => Probleme

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,11 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:quizfulauihapp/question.dart';
+
+import 'answer.dart';
 
 class Quiz extends StatelessWidget {
   // const Quiz({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Question(
+            _question[_questionIndex]['questionText'].toString()),
+        ...(_question[_questionIndex]['answers'] as List<String>)
+            .map((answerChosen) {
+          return Answer(answerQuestion, answerChosen);
+        }).toList(),
+      ],
+    );
   }
 }
